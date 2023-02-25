2ebc5d83bff8e4ce8615cfe0c31d9cb0031e5eb0
mgr-cse <example@dummy.com>
2023-02-25 13:57:55 +0530

    updated for manager testing on broker crash

--- producer/producer.py
+++ producer/producer.py
@@ -17,14 +17,14 @@ res = requests.post('http://' + broker_maganger_ip + ':' + str(broker_manager_po
 
 prod = Producer(broker_maganger_ip, broker_manager_port, 'P-1')
 
-prod.register('T-1', 2)
+prod.register('T-1', 1)
 
 for i in range(30):
     # prepare message
     random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=256))
     timestamp = time.time()
 
-    while not prod.enqueue('T-1', f'debug {i}', timestamp, random_string, 2):
+    while not prod.enqueue('T-1', f'debug {i}', timestamp, random_string, 1):
         time.sleep(1)
     print('message enqueued!')
     time.sleep(1)
