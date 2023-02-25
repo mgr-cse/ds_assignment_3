421f48a4c5e8a8ccf06f2cb64a0923c1f68a59f8
laukikg <laukikg@gmail.com>
2023-02-26 02:25:53 +0530

    added prod to db

--- database.py
+++ database.py
@@ -3,3 +3,14 @@ from typing import Dict
 
 from __main__ import db
 db: SQLAlchemy
+
+class Producer(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    topic_id = db.Column(db.Integer, db.ForeignKey('topic.id'))
+    partition_id = db.Column(db.Integer)
+    
+    health = db.Column(db.Integer)
+    timestamp = db.Column(db.Float)
+
+    def as_dict(self):
+       return {c.name: getattr(self, c.name) for c in self.__table__.columns}
