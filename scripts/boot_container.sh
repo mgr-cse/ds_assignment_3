ca3c31ca340917b570190baedf86a0842f431284
mgr-cse <example@dummy.com>
2023-02-25 13:59:56 +0530

    scripts/boot_container.sh
    added a container start script to start a flask app

---
+++ scripts/boot_container.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+NAME=$1
+FILE=$2
+
+docker start $NAME
+sleep 5
+docker exec -it $NAME /bin/bash -c "sudo -iu mattie /bin/bash -c 'cd $PWD; source 01-env/bin/activate; python $FILE'"
+echo stopping container
+docker stop $NAME
